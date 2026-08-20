d54cb36f38cecbab8be0b2209982ffab2e237c7c
Jhenrique44 <jo-henrique8@hotmail.com>
2026-08-20 14:37:40 -0300

    feat: add route for transactions component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -19,5 +19,10 @@ export const routes: Routes = [
     path: 'dashboard',
     loadComponent: () =>
       import('./pages/dashboard/dashboard.component').then((m) => m.DashboardComponent),
+  },
+  {
+    path: 'transactions',
+    loadComponent: () =>
+      import('./pages/transactions/transactions.component').then((m) => m.TransactionsComponent),
   }
 ];
